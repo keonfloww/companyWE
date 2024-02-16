591d05a68785f653513c9f552491e5ee5261c03f
Tony <Tony>
2024-02-16 21:51:14 +0700

    feat: try catch on syncing mail error

--- src/screens/Inbox/hooks/useInboxScreen.ts
+++ src/screens/Inbox/hooks/useInboxScreen.ts
@@ -67,91 +67,101 @@ const useInboxScreen = () => {
       'handleGetAllMailInConnectedMails targetMail.email',
       targetMail.email,
     );
-    while (true) {
-      let mailAuth: IMailAuth2Params = {
-        access_token: targetMail?.access_token,
-        expiry_date: targetMail.expiry_date,
-        refresh_token: targetMail.refresh_token,
-      };
-
-      const startDate: Moment = moment()
-        .subtract(2, 'week')
-        .set('hour', 0)
-        .set('minute', 0)
-        .set('second', 0);
-
-      const endDate: Moment = moment()
-        .set('hour', 23)
-        .set('minute', 59)
-        .set('second', 59);
-
-      const params: IGetMailParams = {
-        ...mailAuth,
-
-        email_address: targetMail.email,
-        start_date: startDate.unix().toString(),
-        end_date: endDate.unix().toString(),
-        max_results: MAIL_PER_PAGE,
-        next_page_token,
-      };
-      const res = await getMail(params).unwrap();
-      // handle refresh token and retry here
-      const isNeedToRefreshToken = res.token_info.is_expired;
-      if (isNeedToRefreshToken) {
-        next_page_token = next_page_token;
-        mailAuth = {
-          ...mailAuth,
-          access_token: res.token_info.access_token,
-          expiry_date: res.token_info.expiry_date,
+    try {
+      while (true) {
+        let mailAuth: IMailAuth2Params = {
+          access_token: targetMail?.access_token,
+          expiry_date: targetMail.expiry_date,
+          refresh_token: targetMail.refresh_token,
         };
 
+        const startDate: Moment = moment()
+          .subtract(2, 'week')
+          .set('hour', 0)
+          .set('minute', 0)
+          .set('second', 0);
+
+        const endDate: Moment = moment()
+          .set('hour', 23)
+          .set('minute', 59)
+          .set('second', 59);
+
+        const params: IGetMailParams = {
+          ...mailAuth,
+
+          email_address: targetMail.email,
+          start_date: startDate.unix().toString(),
+          end_date: endDate.unix().toString(),
+          max_results: MAIL_PER_PAGE,
+          next_page_token,
+        };
+        const res = await getMail(params).unwrap();
+        // handle refresh token and retry here
+        const isNeedToRefreshToken = res.token_info.is_expired;
+        if (isNeedToRefreshToken) {
+          next_page_token = next_page_token;
+          mailAuth = {
+            ...mailAuth,
+            access_token: res.token_info.access_token,
+            expiry_date: res.token_info.expiry_date,
+          };
+
+          dispatch(
+            userSliceActions.connectedMailUpdateProgress({
+              updatedConnectedMail: {
+                ...targetMail,
+                next_page_token,
+                access_token: res.token_info.access_token,
+                expiry_date: res.token_info.expiry_date,
+              },
+            }),
+          );
+
+          console.log(`${targetMail.email} trigger retry with refresh token`);
+          continue;
+        }
+
+        const isEndOfMatchedMail = next_page_token == res.next_page_token;
+        const isOufOfMail = !res.next_page_token;
+        const isEnd = isEndOfMatchedMail || isOufOfMail;
+        // End of sync the current address mail
+        if (isEnd) {
+          console.log(
+            '---- SYNCED',
+            `${targetMail.email}, from ${startDate.format(
+              DateUtils.FRONTEND_FORMAT_DEFAULT,
+            )} to ${endDate.format(DateUtils.FRONTEND_FORMAT_DEFAULT)}`,
+          );
+
+          dispatch(
+            userSliceActions.connectedMailMarkAsSynced({
+              mail: targetMail.email,
+            }),
+          );
+          handleSetFlagAskForDelete({shouldAsk: true});
+          global?.props?.showDeleteMailModal();
+          break;
+        }
+
+        // update next page to call
         dispatch(
           userSliceActions.connectedMailUpdateProgress({
             updatedConnectedMail: {
               ...targetMail,
-              next_page_token,
-              access_token: res.token_info.access_token,
-              expiry_date: res.token_info.expiry_date,
+              next_page_token: res.next_page_token,
             },
           }),
         );
-
-        console.log(`${targetMail.email} trigger retry with refresh token`);
-        continue;
+        next_page_token = res.next_page_token;
       }
-
-      const isEndOfMatchedMail = next_page_token == res.next_page_token;
-      const isOufOfMail = !res.next_page_token;
-      const isEnd = isEndOfMatchedMail || isOufOfMail;
-      // End of sync the current address mail
-      if (isEnd) {
-        console.log(
-          '---- SYNCED',
-          `${targetMail.email}, from ${startDate.format(
-            DateUtils.FRONTEND_FORMAT_DEFAULT,
-          )} to ${endDate.format(DateUtils.FRONTEND_FORMAT_DEFAULT)}`,
-        );
-
-        dispatch(
-          userSliceActions.connectedMailMarkAsSynced({
-            mail: targetMail.email,
-          }),
-        );
-        handleSetFlagAskForDelete({shouldAsk: true});
-        global?.props?.showDeleteMailModal();
-        break;
-      }
-
-      // update next page to call
+    } catch (error) {
+      console.log('useinboxscreen error 158', error);
+      global.props.showToast('Error in syncing mail', true);
       dispatch(
-        userSliceActions.connectedMailUpdateProgress({
-          updatedConnectedMail: {
-            ...targetMail,
-            next_page_token: res.next_page_token,
-          },
+        userSliceActions.connectedMailMarkAsSynced({
+          mail: targetMail.email,
         }),
       );
-      next_page_token = res.next_page_token;
     }
   };
   // const handleGetAllMailInConnectedMails = async () => {
