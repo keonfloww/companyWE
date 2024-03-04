0d44aadb58383fa634d1ea6a97670c4e858d2161
Tony <Tony>
2024-03-04 21:08:40 +0700

    feat: crashlytics

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
  * @format
  */
 
+// Crashlysis
+import firebase from '@react-native-firebase/app';
+import '@react-native-firebase/crashlytics';
+
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
